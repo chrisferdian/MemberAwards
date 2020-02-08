c10d30fa7fd70dc912e991a1915340d95032868c
Chris Ferdian <chris.ferdian@gmail.com>
2020-02-08 12:42:05 +0700

    Initial Commit

---
+++ MemberAwards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemberAwards
+//
+//  Created by Chris Ferdian on 08/02/20.
+//  Copyright © 2020 Chrizers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
