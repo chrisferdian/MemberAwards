2fdf64e0224ef0aa9958adfdafbf0da0bfc0379d
Chris Ferdian <chris.ferdian@gmail.com>
2020-02-10 08:52:01 +0700

    restructur project

---
+++ MemberAwards/Modules/Welcome/WelcomeModel.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeModel.swift
+//  MemberAwards
+//
+//  Created by Chris Ferdian on 10/02/20.
+//  Copyright © 2020 Chrizers. All rights reserved.
+//
+
+import Foundation
+
+struct WelcomeModel {
+    
+    struct Request {
+        var email: String?
+    }
+    
+    struct Response {
+        var success: Bool?
+    }
+}
---
+++ MemberAwards/Modules/Welcome/WelcomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  WelcomeViewModel.swift
+//  MemberAwards
+//
+//  Created by Chris Ferdian on 10/02/20.
+//  Copyright © 2020 Chrizers. All rights reserved.
+//
+
+import Foundation
+
+class WelcomeViewModel {
+    
+    
+}
